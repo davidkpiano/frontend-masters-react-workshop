bd2b2acd04be9ed99c945f14e4b45068f58448fb
David Khourshid <davidkpiano@gmail.com>
2020-11-04 16:31:05 -0500

    Add alarm

--- src/scratch/ScratchApp.js
+++ src/scratch/ScratchApp.js
@@ -3,5 +3,12 @@ import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
 
 export const ScratchApp = () => {
-  return <div className="scratch">Hello!</div>;
+  return (
+    <div className="scratch">
+      <div className="alarm">
+        <div className="alarmTime">2:16 PM</div>
+        <div className="alarmToggle" data-active></div>
+      </div>
+    </div>
+  );
 };
