7407fa1c0b7c04286c9dd21cbe9809651eef8c83
Dustin Tauer <dustin@dtauer.com>
2021-12-03 09:35:52 -0600

    Resetting index.js
    Since this scratchpad file is used through the XState course, I'm resetting it back to how it looked when the course starts so students can code along with the scratch pad examples.

--- src/scratch/index.js
+++ src/scratch/index.js
@@ -1,173 +1,19 @@
-import React, { useEffect } from 'react';
-import { createMachine, assign, spawn, sendParent } from 'xstate';
-import { useMachine, useService } from '@xstate/react';
-
-const incrementCount = assign({
-  count: (context, event) => {
-    return context.count + 1;
-  },
-});
-
-const notTooMuch = (context, event) => {
-  return context.count < 5;
-};
-
-const greetMachine = createMachine({
-  initial: 'unknown',
-  states: {
-    unknown: {
-      always: [
-        {
-          cond: () => {
-            return new Date().getHours() < 12;
-          },
-          target: 'morning',
-        },
-        { target: 'day' },
-      ],
-    },
-    morning: {},
-    day: {},
-  },
-});
-
-const saveAlarm = async () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res(100);
-    }, 2000);
-  });
-};
-
-const alarmMachine = createMachine(
-  {
-    initial: 'inactive',
-    context: {
-      count: 0,
-    },
-    states: {
-      inactive: {
-        on: {
-          TOGGLE: [
-            {
-              target: 'pending',
-              actions: 'increment',
-              cond: 'notTooMuch',
-            },
-            {
-              target: 'rejected',
-            },
-          ],
-        },
-      },
-      pending: {
-        invoke: {
-          id: 'timeout',
-          src: (context, event) => (sendBack, receive) => {
-            receive((event) => {
-              console.log(event);
-            });
-
-            const timeout = setTimeout(() => {
-              sendBack({
-                type: 'SUCCESS',
-              });
-            }, 2000);
-
-            return () => {
-              clearTimeout(timeout);
-            };
-          },
-          onError: 'rejected',
-        },
-        on: {
-          SUCCESS: 'active',
-          TOGGLE: {
-            target: 'inactive',
-          },
-        },
-      },
-      active: {
-        entry: sendParent('ACTIVE'),
-      },
-      rejected: {},
-    },
-  },
-  {
-    actions: {
-      increment: incrementCount,
-    },
-    guards: {
-      notTooMuch,
-    },
-  }
-);
-
-const Alarm = ({ alarmRef }) => {
-  const [state, send] = useService(alarmRef);
-
-  const status = state.value;
-  const { count } = state.context;
-
-  return (
-    <div className="alarm">
-      <div className="alarmTime">
-        {new Date().toLocaleTimeString('en-US', {
-          hour: '2-digit',
-          minute: '2-digit',
-        })}{' '}
-        ({count}) ({state.toStrings().join(' ')})
-      </div>
-      <div
-        className="alarmToggle"
-        data-active={status === 'active' || undefined}
-        style={{
-          opacity: status === 'pending' ? 0.5 : 1,
-        }}
-        onClick={() => {
-          send('TOGGLE');
-        }}
-      ></div>
-    </div>
-  );
-};
-
-const alarmsMachine = createMachine({
-  context: {
-    alarms: [],
-  },
-  initial: 'active',
-  states: {
-    active: {
-      on: {
-        ADD_ALARM: {
-          actions: assign({
-            alarms: (context, event) => {
-              const alarm = spawn(alarmMachine);
-
-              return context.alarms.concat(alarm);
-            },
-          }),
-        },
-        ACTIVE: {
-          actions: (context, event) => {
-            console.log('Received', event);
-          },
-        },
-      },
-    },
-  },
-});
+import React from 'react';
+import { createMachine } from 'xstate';
+import { useMachine } from '@xstate/react';
 
 export const ScratchApp = () => {
-  const [state, send] = useMachine(alarmsMachine);
-
   return (
     <div className="scratch">
-      <button onClick={() => send('ADD_ALARM')}>Add Alarm</button>
-      {state.context.alarms.map((alarm, i) => {
-        return <Alarm alarmRef={alarm} key={i} />;
-      })}
+      <div className="alarm">
+        <div className="alarmTime">
+          {new Date().toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+          })}
+        </div>
+        <div className="alarmToggle" data-active></div>
+      </div>
     </div>
   );
 };
